a33cc44ef8536c7cd2ea74d5d583ba98ad0fc8c2
Maxime50 <maxime.romero50@gmail.com>
2021-02-20 13:58:57 -0500

    update

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,4 @@
-import { Button, Input, Stack, useToast } from '@chakra-ui/react';
+import { Box, Button, Input, Stack, useToast } from '@chakra-ui/react';
 import { nanoid } from 'nanoid';
 import { useState } from 'react';
 
@@ -29,19 +29,21 @@ function AddTodo({ addTodo }) {
   const [content, setContent] = useState('');
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Stack mt="8" direction={['column', 'row']}>
-        <Input
-          variant="filled"
-          placeholder="Chose à faire"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-        <Button colorScheme="pink" px="8" type="submit">
-          Ajouter
-        </Button>
-      </Stack>
-    </form>
+    <Box width={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '40vw' }}>
+      <form onSubmit={handleSubmit}>
+        <Stack mt="8" direction={['column', 'row']}>
+          <Input
+            variant="filled"
+            placeholder="Chose à faire"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <Button colorScheme="pink" px="8" type="submit">
+            Ajouter
+          </Button>
+        </Stack>
+      </form>
+    </Box>
   );
 }
 
